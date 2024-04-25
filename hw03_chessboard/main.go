22c6acd2e138ce1647df1d8177f0cb340f413f40
babanakov84 <165267460+babanakov84@users.noreply.github.com>
2024-04-25 13:00:06 +0300

    HW3 завершено. Наверняка можно сделать проще, но я только учусь)

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var a, b int
+	fmt.Println("Введите количество строк:")
+	fmt.Scanf("%d\n", &a)
+	fmt.Println("Введите количество столбцов:")
+	fmt.Scanf("%d\n", &b)
+
+	for i := 1; i <= a; i++ {
+		switch i%2 == 1 {
+		case true:
+			for j := 1; j <= b; j++ {
+				switch j%2 == 1 {
+				case true:
+					fmt.Printf("%3v", "X")
+				default:
+					fmt.Printf("%3v", " ")
+				}
+			}
+			fmt.Printf("\n")
+		case false:
+			for j := 1; j <= b; j++ {
+				switch j%2 == 1 {
+				case true:
+					fmt.Printf("%3v", " ")
+				default:
+					fmt.Printf("%3v", "X")
+				}
+			}
+			fmt.Printf("\n")
+		}
+	}
 }
